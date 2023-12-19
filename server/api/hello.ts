0f8aca8fb73084ea186dc2bca874a6c33ef305b9
Ryoya-Fukasawa <fukasawa.ryoya@gmail.com>
2023-12-19 13:41:03 +0900

    update api

--- server/api/hello.ts
+++ server/api/hello.ts
@@ -1,5 +1,12 @@
-export default defineEventHandler((event) => {
-    return {
-      api: 'works'
-    }
-  })
+import axios from 'axios';
+
+export default defineEventHandler(async (event) => {
+  try {
+    const response = await axios.get('https://fortune.line.me/contents/horoscope/');
+
+    return response.data;
+  } catch (error: any) {
+    console.error(`エラーが発生しました: ${error.message}`);
+    return error;
+  }
+});
