5131774bb117eac91bad92f8cfb660b624181825
Ryoya-Fukasawa <fukasawa.ryoya@gmail.com>
2023-12-19 14:03:58 +0900

    modify api

--- server/api/hello.ts
+++ server/api/hello.ts
@@ -1,12 +1,13 @@
 import axios from 'axios';
 
 export default defineEventHandler(async (event) => {
-  try {
-    const response = await axios.get('https://fortune.line.me/contents/horoscope/');
+  // try {
+  //   const response = await axios.get('https://fortune.line.me/contents/horoscope/');
 
-    return response.data;
-  } catch (error: any) {
-    console.error(`エラーが発生しました: ${error.message}`);
-    return error;
-  }
+  //   return response.data;
+  // } catch (error: any) {
+  //   console.error(`エラーが発生しました: ${error.message}`);
+  //   return error;
+  // }
+  return { message: 'Hello, World!' };
 });
